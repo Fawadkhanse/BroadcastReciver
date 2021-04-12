294e1fbfe3c2d05a7af4a52fac8d5d2e8cfa10d3
Fawadkhan <Fawadkhanse@gmail.com>
2021-04-12 17:43:35 +0500

    include the logcat

--- app/src/main/java/com/fawadkhanse/broadcastreciver/MyReceiver.java
+++ app/src/main/java/com/fawadkhanse/broadcastreciver/MyReceiver.java
@@ -3,14 +3,17 @@ package com.fawadkhanse.broadcastreciver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MyReceiver extends BroadcastReceiver {
+    private static final String TAG = "MyReceiver";
 
     @Override
     public void onReceive(Context context, Intent intent) {
       // Toast massage
         Toast.makeText(context,"BroadCast Recived",Toast.LENGTH_LONG).show();
+        Log.d ( TAG, "onReceive: log for testing " );
 
 //    ///  2nd mathoad for toast massage
 //        Toast Mytoast;
